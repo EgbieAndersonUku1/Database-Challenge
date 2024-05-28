680fc0ae0e123b0795412861151e3d2e0c5b4b9a
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2024-05-28 23:51:10 +0100

    Completed Challenge 1 to 4

--- src/model/products.js
+++ src/model/products.js
@@ -1,1 +1,65 @@
 const db = require("../database/db.js");
+
+
+const select_categories = db.prepare(/*sql*/ `
+  SELECT * FROM products
+`);
+
+
+const select_product_by_id = db.prepare(/*sql*/ `
+    SELECT id, name FROM products WHERE id = ?
+`);
+
+
+const searchProductByName = db.prepare(/*sql*/ `
+    SELECT id, name FROM products WHERE name LIKE ?
+`);
+
+
+
+const getNameAndDescriptionFromCategory = db.prepare(/*sql*/ `
+    SELECT name AS category_name, description AS category_description FROM categories WHERE id = ?
+`);
+
+
+
+function listProducts() {
+  const products = select_categories.all();
+  // add the ability for challeng
+  return select_categories.all();
+
+
+}
+
+
+function getProduct(id) {
+
+    const product = select_product_by_id.get(id);
+
+    if (product && product.id) {
+
+         const category = getNameAndDescriptionFromCategory.get(product.id);
+         Object.keys(category).forEach((key) => {
+            product[key] = category[key];
+         })
+    
+         return product;
+    }
+   
+    return product;
+};
+
+
+function searchProducts(searchParameter) {
+
+    const searchQuery = `%${searchParameter.toUpperCase()}%`;
+    return searchProductByName.all(searchQuery);
+};
+
+
+module.exports = { 
+    listProducts,
+    getProduct,
+    searchProducts,
+ };
+
